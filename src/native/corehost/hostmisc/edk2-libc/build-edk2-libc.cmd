140fc8147389cfd6a8944b8148bd3c0c04f0510f
Takym <15681312+Takym@users.noreply.github.com>
2022-05-05 08:23:54 +0900

    05/05 - OS.NET

--- src/native/corehost/hostmisc/edk2-libc/build-edk2-libc.cmd
+++ src/native/corehost/hostmisc/edk2-libc/build-edk2-libc.cmd
@@ -9,16 +9,16 @@ set EDK_TOOLS_BIN=%MODULES%edk2-BaseTools-win32
 set WORKSPACE=%~dp0temp
 set CONF_PATH=%~dp0
 
-xcopy /E /Y "%MODULES%edk2\ArmPkg"                          "%WORKSPACE%\ArmPkg\"
+if exist "%WORKSPACE%\" goto build
 xcopy /E /Y "%MODULES%edk2\MdeModulePkg"                    "%WORKSPACE%\MdeModulePkg\"
 xcopy /E /Y "%MODULES%edk2\MdePkg"                          "%WORKSPACE%\MdePkg\"
 xcopy /E /Y "%MODULES%edk2\ShellPkg"                        "%WORKSPACE%\ShellPkg\"
 xcopy /E /Y "%MODULES%edk2-libc\AppPkg"                     "%WORKSPACE%\AppPkg\"
 xcopy /E /Y "%MODULES%edk2-libc\StdLib"                     "%WORKSPACE%\StdLib\"
 xcopy /E /Y "%MODULES%edk2-libc\StdLibPrivateInternalFiles" "%WORKSPACE%\StdLibPrivateInternalFiles\"
+
+:build
 call %MODULES%edk2/edksetup.bat
 call build
-
-rem rmdir /S /Q %WORKSPACE%
 
 endlocal
